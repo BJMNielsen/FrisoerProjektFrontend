129c986b2872280f50e3fc962e3d2ae3a0b41ecb
mauf8 <marc032n@stud.kea.dk>
2023-05-10 11:18:51 +0200

    fixet priser mm.

---
+++ JavaScript/FetchPriser.js
@@ -0,0 +1,31 @@
+console.log("Jeg er inde i Fetch Priser")
+const tblTreatments = document.querySelector("#tblTreatments")
+
+//dette er ligesom en lambda - en anonymous function
+function intetNavnHar(treatments) {
+
+}
+
+function createRowsForTreatmentTable(treatment){
+    const row = document.createElement("tr")
+    row.innerHTML= `
+    <td>${treatment.name}</td>
+    <td>${treatment.price}</td>
+    
+    `;
+    tblTreatments.appendChild(row)
+}
+
+function fetchTreatments(){
+    tblTreatments.innerHTML = ''
+    getLocalEntities("treatments").then(treatments => {
+        // de skal ind i vores tabel
+        treatments.forEach(treatment => {
+            createRowsForTreatmentTable(treatment)
+        })
+    }).catch(error =>{
+        alert("there was an error retrieving the treatments, Error: " + error.message)
+    })
+}
+
+fetchTreatments()
---
+++ JavaScript/userProfilePage.js
@@ -0,0 +1,7 @@
+console.log("jeg er i userprofilePage javascript")
+    const bookingsTable = document.querySelector("#tblBooking")
+
+function fetchBookingsFromUser(){
+    bookingsTable.innerHTML = ''
+
+}
