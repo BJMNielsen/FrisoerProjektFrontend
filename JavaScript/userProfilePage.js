ab9271bc29aa5cea8b117df9eaed26e635ce5036
haugaard1101 <haugaard1101@gmail.com>
2023-05-15 11:21:53 +0200

    trying to add delete to userprofilepage

--- JavaScript/userProfilePage.js
+++ JavaScript/userProfilePage.js
@@ -14,6 +14,9 @@ function updateUserprofileInformation(){
    document.querySelector("#Email").innerHTML = email;
    document.querySelector("#phoneNumber").innerHTML = phoneNumber;
 
+    document.querySelector("#tblBooking").addEventListener("click", deleteBookingButtonEvent);
+
+
 }
 
 
@@ -45,6 +48,8 @@ function createRowsForBookingTable(booking){
     <td>${booking.timeSlot.endTime}</td>
     <td>${treatmentNames.join(", ")}</td>
     <td>${totalPrice}</td>
+    <td><button class="btn btn-primary" id="${booking.id}-deleteBooking">Delete</button></td>
+
   `;
     bookingsTable.appendChild(row);
 }
@@ -62,4 +67,51 @@ fetchAny("bookings/userid/" + userId,"GET",null).then(userBookings=>{
     alert(error.message)
 })
 
+/*
+async function deleteBookingButtonEvent(event) {
+    // Vi sender en event, som er any "click" på hjemmesiden.
+    // Target er det vi klikker på.
+    const target = event.target;
+    // Vores console.log af const target, bliver så den knap vi klikker på. <button class="btn btn-primary" id="1-deleteBooking">Delete</button>
+    // Dvs target er vores delete knap, hvis vi trykker på den.
+    console.log(target)
+    if (!target.classList.contains("btn"))
+        // Vi siger så at hvis target ikke indeholder "btn" i class tag, så gør vi ikke noget.
+        return;
+    else {
+        // Ellers tager vi knappens id, finder "-deleteBooking" delen af id'et og sletter.
+        // Vi har givet hver knap et id der hedder "id + -deleteBooking", hvor id er det samme id som hver bookningen har.
+        // Dvs for at få fat i bookningens id som vi kan lave et fetch delete kald på til vores backend, så fjerner vi "-deleteBooking" fra knapnavnet.
+        const id = target.id.replace("-deleteBooking", "");
+        console.log(id);
+        deleteBookingById(id);
+        // Vi refresher vores tabel
+        window.location.reload();
+    }
 
+    function deleteBookingById(bookingId) {
+        // Vi laver et fetch kald, med vores id, og specifier metoden til at være DELETE
+        fetchAny("bookings/bookingId/" + bookingId,"DELETE",null)
+            // Her siger vi at responset vi får fra vores backend
+            // (hvor vi jo har lavet et responseEntity der returner det vi sletter og statuskoden)
+            // det printer vi. Vi håndtere ligesom svaret fra vores backend server.
+            .then(response => {
+                if (response.ok) {
+                    // Vi laver vores response om til json.
+                    // Vi tager så dataen i den json (.then(data => {...
+                    // og console logger vores deleted booking og statuscoden, og returner det.
+                    return response.json().then(data => {
+                        console.log("Deleted booking:", data);
+                        console.log("Status code:", response.status);
+                    });
+                } else {
+                    throw new Error("HTTP status code: " + response.status);
+                }
+            })
+            .catch(error => {
+                console.log("An error occurred:", error);
+            });
+
+    }
+}
+*/
